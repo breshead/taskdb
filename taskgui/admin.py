8425c21ca5b0ecc487dc82f1b473c0f31fc66ed8
breshead <doug@jshfarms.com>
2016-11-11 16:37:39 -0800

    Make metadata tables editable by admin

--- taskgui/admin.py
+++ taskgui/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from taskgui.models import status
+from taskgui.models import ttype
+from taskgui.models import location
+from taskgui.models import department
+from taskgui.models import user_pref
+
+
+admin.site.register(status)
+admin.site.register(ttype)
+admin.site.register(location)
+admin.site.register(department)
+admin.site.register(user_pref)
+
